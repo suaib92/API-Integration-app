304369dce0f2a65d409d382d77c78e7743f34ae1
suaib <suaib8211@gmail.com>
2024-06-06 23:03:07 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { fetchPosts } from './services/api';
 import Header from './components/Header';
 import PostTable from './components/PostTable';
-import { Container } from '@mui/material';
+import { Container, CircularProgress, Box } from '@mui/material';
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(true); // Added state for loading
 
   useEffect(() => {
     const getPosts = async () => {
       const posts = await fetchPosts();
       setPosts(posts);
+      setLoading(false); // Set loading to false after data is fetched
     };
     getPosts();
   }, []);
@@ -28,7 +30,13 @@ function App() {
     <div className="bg-gray-100 min-h-screen flex flex-col">
       <Header searchTerm={searchTerm} handleSearch={handleSearch} />
       <Container className="flex-grow">
-        <PostTable posts={filteredPosts} />
+        {loading ? (
+          <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+            <CircularProgress />
+          </Box>
+        ) : (
+          <PostTable posts={filteredPosts} />
+        )}
       </Container>
     </div>
   );
